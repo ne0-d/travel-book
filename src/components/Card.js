e6d2127c892b204690451fec4eeeb284cf4ba40e
ne0-d <dhruvbhati2001@gmail.com>
2022-07-26 15:04:59 +0530

    Completed static card component.

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,12 @@ export default function Card(props){
     return(
 
         <div class="card">
-            <img className="location--image" src={props.item.imageUrl} />
+            <img className="card--image" src={props.item.imageUrl} />
             <div class="info">
-                <p>Japan <a href="">View on Google Maps</a></p>
-                <h2>Mount Fuji</h2>
+                <p className="card--location"><img src="/images/location.svg" className="location-logo" />Japan <a href="">View on Google Maps</a></p>
+                <h2 className="card--title">Mount Fuji</h2>
                 <p className="card--duration">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </div>
     );
