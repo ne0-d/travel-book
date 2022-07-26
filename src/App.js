27cf34fd0dbb937074ee6c4acc7ef30ef64ba7c4
ne0-d <dhruvbhati2001@gmail.com>
2022-07-26 15:32:36 +0530

    Made the card dynamic(kind of lol)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Card from "./components/Card"
 import data from "./data"
 import './App.css';
 const card = data.map((item)=>{
+  
   return(
     <Card
       item = {item}/>
-  )
-})
+  );
+});
 function App() {
   return (
     <div className="container">
       <Navbar />
-      {card}
+      <div class="card--list">{card}</div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,16 @@
 export default function Card(props){
-    console.log(props)
+    let i = props.item.length;
+    console.log(i);
     return(
-
         <div class="card">
             <img className="card--image" src={props.item.imageUrl} />
             <div class="info">
-                <p className="card--location"><img src="/images/location.svg" className="location-logo" />Japan <a href="">View on Google Maps</a></p>
-                <h2 className="card--title">Mount Fuji</h2>
-                <p className="card--duration">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="card--description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <p className="card--location"><img src="/images/location.svg" className="location-logo" />{props.item.location} <a href={props.item.googleMapsUrl}>View on Google Maps</a></p>
+                <h2 className="card--title">{props.item.title}</h2>
+                <p className="card--duration">{props.item.startDate} - {props.item.endDate}</p>
+                <p className="card--description">{props.item.description}</p>
             </div>
+            <br></br>
         </div>
     );
 }
